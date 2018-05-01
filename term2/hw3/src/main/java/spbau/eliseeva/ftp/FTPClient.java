9519f2ee3cee98db6c4fe09c7faaf3437b046d7f
Marieliseeva <eliseevamary@mail.ru>
2018-05-01 23:41:02 +0300

    add better reading and writing

--- term2/hw3/src/main/java/spbau/eliseeva/ftp/FTPClient.java
+++ term2/hw3/src/main/java/spbau/eliseeva/ftp/FTPClient.java
@@ -14,22 +14,69 @@ public class FTPClient {
         String hostName = scanner.nextLine();
         System.out.print("Write port number.\n");
         int portNumber = Integer.parseInt(scanner.nextLine());
+        try {
+            checkConnection(hostName, portNumber);
+            communicateWithServer(scanner, hostName, portNumber);
+        } catch (IOException e) {
+            System.err.println("Problems with connection or reading and writing.");
+        }
+    }
 
-        Socket socket = null;
+    private static void communicateWithServer(Scanner scanner, String hostName, int portNumber) throws IOException {
         String fromUser = scanner.nextLine();
         while (!fromUser.equals("exit")) {
-            try {
-                socket = new Socket(InetAddress.getByName(hostName), portNumber);
-                DataInputStream in = new DataInputStream(socket.getInputStream());
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                out.writeUTF(fromUser);
-                out.flush();
-                String fromServer = in.readUTF();
-                System.out.println("Server: " + fromServer);
-                fromUser = (new Scanner(System.in)).nextLine();
-            } catch (IOException e) {
-                e.printStackTrace();
+            Socket socket = new Socket(InetAddress.getByName(hostName), portNumber);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            if (fromUser.charAt(0) != '1' && fromUser.charAt(0) != '2') {
+                System.out.println("Wrong command.");
+                continue;
             }
+            out.writeInt(fromUser.charAt(0) - '0');
+            out.writeUTF(fromUser.split(" ")[1]);
+            out.flush();
+            if (fromUser.charAt(0) == '1') {
+                getAnswer(in);
+            } else if (fromUser.charAt(0) == '2') {
+                listAnswer(in);
+            }
+            fromUser = scanner.nextLine();
         }
+    }
+
+    private static void getAnswer(DataInputStream in) throws IOException {
+        int size = in.readInt();
+        System.out.print(size);
+        System.out.print(' ');
+        for (int i = 0; i < size; i++) {
+            System.out.print('(' + in.readUTF() + ' ');
+            System.out.print(in.readBoolean());
+            System.out.print(')');
+        }
+        System.out.println();
+    }
+
+    private static void listAnswer(DataInputStream in) throws IOException {
+        long size = in.readLong();
+        System.out.print(size);
+        if (size != 0) {
+            System.out.print(' ');
+            int c;
+            byte[] buffer = new byte[1024];
+            while ((c = in.read(buffer)) != -1) {
+                System.out.write(buffer, 0, c);
+            }
+        } else {
+            System.out.println();
+        }
+    }
+
+    private static void checkConnection(String hostName, int portNumber) throws IOException {
+        Socket socket = new Socket(InetAddress.getByName(hostName), portNumber);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        out.writeInt(17);
+        out.flush();
+        System.out.println(in.readUTF());
     }
 }
--- term2/hw3/src/main/java/spbau/eliseeva/ftp/FTPServer.java
+++ term2/hw3/src/main/java/spbau/eliseeva/ftp/FTPServer.java
@@ -28,15 +28,14 @@ public class FTPServer {
             ServerSocket serverSocket = new ServerSocket(portNumber);
             while (!Thread.interrupted()) {
                 Socket clientSocket = serverSocket.accept();
-                FTPCommunication ftpCommunication = new FTPCommunication();
                 Thread thread = new Thread(() -> {
                     try {
                         DataInputStream in = new DataInputStream(clientSocket.getInputStream());
                         DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-                        ftpCommunication.processInput(in, out);
+                        processInput(in, out);
                         out.flush();
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        System.err.println("Problems with connection or reading and writing.");
                     }
                 });
                 thread.setDaemon(true);
@@ -46,72 +45,58 @@ public class FTPServer {
         }
     }
 
-    private class FTPCommunication {
-        public void processInput(DataInputStream dataInputStream, DataOutputStream dataOutputStream) {
-            String input = null;
-            try {
-                input = dataInputStream.readUTF();
-            } catch (IOException ignored) {
+    private void processInput(DataInputStream dataInputStream, DataOutputStream dataOutputStream) throws IOException {
+            int command = dataInputStream.readInt();
+            switch (command) {
+                case 1:
+                    list(dataInputStream.readUTF(), dataOutputStream);
+                case 2:
+                    get(dataInputStream.readUTF(), dataOutputStream);
+                case 17:
+                    dataOutputStream.writeUTF("connected");
+                default:
+                    dataOutputStream.writeUTF("wrong command.");
             }
-            assert input != null;
-            if (input.charAt(0) == '1') {
-                try {
-                    list(input.split(" ")[1], dataOutputStream);
-                } catch (IOException ignored) {
-                }
-                return;
-            }
-            if (input.charAt(0) == '2') {
-                try {
-                    get(input.split(" ")[1], dataOutputStream);
-                } catch (IOException ignored) {
-                }
-                return;
-            }
-            try {
-                dataOutputStream.writeUTF("wrong command.");
-            } catch (IOException ignored) {
-            }
-        }
+    }
 
-        private void get(String fileName, DataOutputStream dataOutputStream) throws IOException {
-            FileInputStream fileInputStream;
-            try {
-                fileInputStream = new FileInputStream(fileName);
-            } catch (FileNotFoundException e) {
-                dataOutputStream.writeUTF("0");
-                return;
-            }
-            int size = 0;
-            int length;
-            byte[] buffer = new byte[1024];
-            StringBuilder answer = new StringBuilder();
-            while ((length = fileInputStream.read(buffer)) != -1) {
-                size += length;
-            }
-            answer.append(String.valueOf(size));
-            fileInputStream.close();
+    private void get(String fileName, DataOutputStream dataOutputStream) throws IOException {
+        FileInputStream fileInputStream;
+        try {
             fileInputStream = new FileInputStream(fileName);
-            while (fileInputStream.read(buffer) != -1) {
-                for (byte c : buffer) {
-                   answer.append((char) c);
-                }
-            }
-            dataOutputStream.writeUTF(answer.toString());
-            fileInputStream.close();
+        } catch (FileNotFoundException e) {
+            dataOutputStream.writeLong(0);
+            return;
         }
+        if ((new File(fileName)).isDirectory()) {
+            dataOutputStream.writeLong(0);
+            return;
+        }
+        int size = 0;
+        int length;
+        byte[] buffer = new byte[1024];
+        while ((length = fileInputStream.read(buffer)) != -1) {
+            size += length;
+        }
+        dataOutputStream.writeLong(size);
+        fileInputStream.close();
+        fileInputStream = new FileInputStream(fileName);
+        int c;
+        while ((c = fileInputStream.read(buffer)) != -1) {
+            dataOutputStream.write(buffer, 0, c);
+        }
+        fileInputStream.close();
+    }
 
-        private void list(String directoryName, DataOutputStream dataOutputStream) throws IOException {
-            File dir = new File(directoryName);
-            if (!dir.isDirectory()) {
-                dataOutputStream.writeUTF("0\n");
-                return;
-            }
-            StringBuilder answer = new StringBuilder(dir.listFiles().length + " ");
-            for (File file : dir.listFiles()) {
-                answer.append("(").append(file.getName()).append(" ").append(file.isDirectory()).append(")");
-            }
-            dataOutputStream.writeUTF(answer + "\n");
+    private void list(String directoryName, DataOutputStream dataOutputStream) throws IOException {
+        File dir = new File(directoryName);
+        if (!dir.isDirectory()) {
+            dataOutputStream.writeInt(0);
+            return;
+        }
+        dataOutputStream.writeInt(dir.listFiles().length);
+        for (File file : dir.listFiles()) {
+            dataOutputStream.writeUTF(file.getName());
+            dataOutputStream.writeBoolean(file.isDirectory());
         }
     }
 }
